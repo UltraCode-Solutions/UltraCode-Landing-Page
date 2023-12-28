cb1b01f41b4efbca5300b78133d246253d0f8e26
Skarvy <skarvy1@gmail.com>
2023-12-28 19:50:15 -0300

    Footer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+
 import Navbar from "../components/Navbar/Navbar.jsx";
+import Footer from "@/components/Footer/Footer.jsx";
 export default function Home() {
    return (
       <main className="flex min-h-screen flex-col bg-[#ffffff]">
          <Navbar />
+         <Footer/>
       </main>
    );
 }

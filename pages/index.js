ba694abc8228184964b9eb71f28941dc1dcb1157
Futuredevkev <kevinsitosoyyoguadateamo1212@outlook.com>
2023-12-28 20:21:08 -0300

    hero

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Hero from "@/components/hero/Hero.jsx";
 import Navbar from "../components/Navbar/Navbar.jsx";
 export default function Home() {
    return (
       <main className="flex min-h-screen flex-col bg-[#ffffff]">
          <Navbar />
+         <Hero />
       </main>
    );
 }

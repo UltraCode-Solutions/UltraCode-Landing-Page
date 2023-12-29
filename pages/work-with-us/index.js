29b864b8adf0c9d309c45b99e281c019a874610f
Luciano Griffa <lucianogriffa@protonmail.com>
2023-12-29 09:00:26 -0300

    feat: Add 'Work with us' Page and integrate NavBar and Footer Components

---
+++ pages/work-with-us/index.js
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
+
+export default function WorkWithUs() {
+  return <div className="flex min-h-screen flex-col bg-[#ffffff]">
+    <Navbar/>
+    <Footer/>
+  </div>
+}

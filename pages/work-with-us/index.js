bb71fe625fa3627176f469e79c529a0c9156dfca
Luciano Griffa <lucianogriffa@protonmail.com>
2023-12-29 09:35:34 -0300

    feat: Add hero section to 'Work with us' page

--- pages/work-with-us/index.js
+++ pages/work-with-us/index.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
+import HeroWork from "@/components/HeroWorkWithUs/HeroWork";
 import Footer from "@/components/Footer/Footer";
 
 export default function WorkWithUs() {
   return <div className="flex min-h-screen flex-col bg-[#ffffff]">
     <Navbar/>
+    <HeroWork/>
     <Footer/>
   </div>
 }
